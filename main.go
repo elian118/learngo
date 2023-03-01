4a16f9cf33652a22c9fc3bb76d9bc6420e65cb78
elian <elian118@naver.com>
2023-03-01 20:55:11 +0900

    1.4 Functions part Two

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func lenAndUpper(name string) (int, string) {
+	defer fmt.Println("function lenAndUpper done.") // defer -> 함수 종료 후 의무 실행
 	return len(name), strings.ToUpper(name)
+}
+
+// lenAndUpper()와 결과 동일
+func lenAndUpper2(name string) (length int, uppercase string) {
+	defer fmt.Println("function lenAndUpper2 done.") // defer -> 함수 종료 후 의무 실행
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func multiply(a int, b int) int {
@@ -28,10 +37,12 @@ func main() {
 	fmt.Println("---------------")
 	fmt.Println(multiply(2, 2)) // 4
 	fmt.Println("---------------")
-	//totalLength, upperName := lenAndUpper("elianus")
-	totalLength, _ := lenAndUpper("elianus") // 언더스코어 -> 무시
-	//fmt.Println(totalLength, upperName) // 7 ELIANUS
-	fmt.Println(totalLength) // 7
+	totalLength, upperName := lenAndUpper("elianus")
+	// totalLength, _ := lenAndUpper("elianus") // 언더스코어 -> 무시
+	fmt.Println(totalLength, upperName) // 7 ELIANUS
+	// fmt.Println(totalLength)                            // 7
+	totalLength2, upperName2 := lenAndUpper2("elianus2") // 언더스코어 -> 무시
+	fmt.Println(totalLength2, upperName2)                // 8 ELIANUS2
 	fmt.Println("---------------")
 	repeatMe("nico", "lynn", "dal", "marl", "flynn") // [nico lynn dal marl flynn]
 }
