09c69119f0943a2e0fb0b37e0456658171e162ab
elian <elian118@naver.com>
2023-03-01 20:21:31 +0900

    1.2 Variables and Constants

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main // 컴퓨터가 컴파일하는 파일은 오직 main 패키지 뿐이다.
 
-import (
-	"fmt"
-	"github.com/elian/learngo/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
-	something.SayHello()
+	const val string = "val"
+	// 축약형 변수 선언 => 오직 func 안에서만 작동
+	name := "elian" // var name string = "elian" -> 입력된 값의 타입으로부터 타입유추 -> string 간주
+	name = "lynn"
+	fmt.Println(val)
+	fmt.Println(name)
 }
