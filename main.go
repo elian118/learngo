4c392b254a7123f0685e5e51c737bf7a1a0fb2e1
elian <elian118@naver.com>
2023-03-01 20:39:13 +0900

    1.3 Functions part One

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main // 컴퓨터가 컴파일하는 파일은 오직 main 패키지 뿐이다.
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
 	const val string = "val"
 	// 축약형 변수 선언 => 오직 func 안에서만 작동
 	name := "elian" // var name string = "elian" -> 입력된 값의 타입으로부터 타입유추 -> string 간주
 	name = "lynn"
-	fmt.Println(val)
-	fmt.Println(name)
+	fmt.Println("---------------")
+	fmt.Println(val)  // val
+	fmt.Println(name) // lynn
+	fmt.Println("---------------")
+	fmt.Println(multiply(2, 2)) // 4
+	fmt.Println("---------------")
+	//totalLength, upperName := lenAndUpper("elianus")
+	totalLength, _ := lenAndUpper("elianus") // 언더스코어 -> 무시
+	//fmt.Println(totalLength, upperName) // 7 ELIANUS
+	fmt.Println(totalLength) // 7
+	fmt.Println("---------------")
+	repeatMe("nico", "lynn", "dal", "marl", "flynn") // [nico lynn dal marl flynn]
 }
